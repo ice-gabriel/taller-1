2979c2b899e6513bc3754eb6021e432ef2985a4d
ice-gabriel15 <thiagozuazo15t@gmail.com>
2021-05-13 16:53:17 -0300

    subo repaso3

--- thiago gabriel zuazo (copia)/ciclos_interacciones/index.js
+++ thiago gabriel zuazo (copia)/ciclos_interacciones/index.js
@@ -11,10 +11,67 @@ boton.addEventListener("click",calcular1)
 const recorrer_arreglo = () => {
     let miarreglo2 = ['programacion1','web2','robotica']
 
-    for(indice in miarreglo){
+    for(indice in miarreglo2){
         console.log( miarreglo2[indice] )
     }
 }
 
 const boton2 = document.querySelector("#btn_recorrer")
 boton2.addEventListener("click",recorrer_arreglo)
+
+
+
+
+
+
+
+let miobjeto = {
+    nombre:'pepe',
+    correo:{
+        correo1:'pepito@gmail.com',
+        correo2:'pepito@outlook.com'
+    },
+    redes_sociales:{
+        facebook:'www.facebook.com/pepe',
+        twitter:'www.facebook.com/pepe'
+    }
+
+    
+
+}
+
+console.log(`
+    mi nombre es: ${miobjeto.nombre}
+    mi facebook es: ${miobjeto.redes_sociales.twitter}
+    
+    `);
+
+
+
+
+
+    const recorrer_objetos = [
+        {
+            nombre:'pepe',
+            apellido:'argento'
+        },
+        {
+            nombre:'juan',
+            apellido:'perez'
+        }
+    ]
+
+
+
+    recorrer_objetos.forEach(objeto =>{
+        console.log(
+            `
+            nombre: ${objeto.nombre}
+            apellido: ${objeto.apellido}
+            
+            `
+        )
+    });
+
+    const boton4 = document.querySelector("#btn_recorrer_objetos")
+    boton4.addEventListener("click",recorrer_objetos)
