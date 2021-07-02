53c995a0727ff8db5777814b4fc57660df798378
ice-gabriel <thiagozuazo15@gmail.com>
2021-07-02 15:42:33 -0300

    subo punto 3

--- tp_evaluativo/punto 3/index.js
+++ tp_evaluativo/punto 3/index.js
@@ -8,31 +8,25 @@
 
 function pitagoras(a, b){
 
-
-
-let A = Math.pow(a, 2)
-let B = parseInt(2) * parseInt(9.8) * parseInt(b)
-let suma = parseInt(A) + parseInt(B)
-
-
-let raiz = Math.sqrt(suma)
-
-
-return raiz
-}
+    a2 = Math.pow(a, 2)
+    c = (2*9.8*b)
+    suma = parseInt(a2) + parseInt(c)
+    raiz = Math.sqrt(suma)
+    return raiz
+    }
+        
+        
     
     
-
-
-
-function imprimir(){
-    const a = document.getElementById('inp_co').value;
-    const b = document.getElementById('inp_ca').value;
-    let raiz_R = pitagoras(a, b)
-    document.getElementById("RH").textContent = raiz_R
-}
-
-
-const boton = document.querySelector("#btn_p")
-
-boton.addEventListener("click",imprimir)
+    
+    function imprimir(){
+        const a = document.getElementById('inp_vi').value;
+        const b = document.getElementById('inp_h').value;
+        let raiz_R = pitagoras(a, b)
+        document.getElementById("v_vf").textContent = raiz_R
+    }
+    
+    
+    const boton = document.querySelector("#btn_p")
+    
+    boton.addEventListener("click",imprimir)
